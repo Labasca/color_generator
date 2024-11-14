221161d306a355ee390927bcba1d8c3bf2e4bf95
Domas <116216430+Labasca@users.noreply.github.com>
2024-11-14 23:24:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import plotly.graph_objects as go
 import colorsys
 
+
 def hsl_to_hex(h, s, l):
     # Convert HSL to a range used by colorsys (h between 0 and 1, s and l between 0 and 1)
     h = h / 360
@@ -11,40 +12,85 @@ def hsl_to_hex(h, s, l):
     # Convert RGB to HEX
     return f"#{int(r * 255):02x}{int(g * 255):02x}{int(b * 255):02x}"
 
-def generate_hex_colors(hue, num_colors, saturation_position, lightness_ends=[95, 80]):
-    # Define the starting lightness level
-    lightness_start = 20
+
+def get_adjusted_lightness_ranges(num_colors):
+    # Adjust lightness ranges based on number of colors
+    if num_colors <= 3:
+        return {
+            'grey_start': 30,
+            'grey_end': 85,
+            'color_start': 40,
+            'color_end': 75
+        }
+    elif num_colors <= 5:
+        return {
+            'grey_start': 25,
+            'grey_end': 90,
+            'color_start': 35,
+            'color_end': 80
+        }
+    else:
+        return {
+            'grey_start': 20,
+            'grey_end': 95,
+            'color_start': 35,
+            'color_end': 80
+        }
+
+
+def generate_hex_colors(hue, num_colors, saturation_position):
+    # Get adjusted lightness ranges based on number of colors
+    ranges = get_adjusted_lightness_ranges(num_colors)
 
     # Saturation values are ordered to generate grey colors first
     saturation_values = [0, saturation_position]
 
     # Adjust distribution when total colors aren't evenly divisible
-    # The second saturation group gets the extra color when num_colors is odd
     num_colors_per_saturation = [num_colors // 2 + (num_colors % 2 > i) for i in range(len(saturation_values))][::-1]
 
     # Generate the color list
     colors = []
-    for idx, (saturation, lightness_end) in enumerate(zip(saturation_values, lightness_ends)):
-        step = (lightness_end - lightness_start) / (num_colors_per_saturation[idx] - 1)
+    for idx, saturation in enumerate(saturation_values):
+        if saturation == 0:
+            # Grey colors
+            lightness_start = ranges['grey_start']
+            lightness_end = ranges['grey_end']
+        else:
+            # Colorful colors
+            lightness_start = ranges['color_start']
+            lightness_end = ranges['color_end']
+
+        num_in_group = num_colors_per_saturation[idx]
+
+        # Handle special case when only 1 color in group
+        if num_in_group == 1:
+            # Use middle lightness value for single colors
+            middle_lightness = (lightness_start + lightness_end) / 2
+            colors.append(hsl_to_hex(hue, saturation, middle_lightness))
+            continue
+
+        step = (lightness_end - lightness_start) / (num_in_group - 1)
+
         if saturation == 0:
             # Normal progression for 0% saturation
             colors += [
                 hsl_to_hex(hue, saturation, lightness_start + i * step)
-                for i in range(num_colors_per_saturation[idx])
+                for i in range(num_in_group)
             ]
         else:
             # Reverse progression for saturated colors
             colors += [
                 hsl_to_hex(hue, saturation, lightness_end - i * step)
-                for i in range(num_colors_per_saturation[idx])
+                for i in range(num_in_group)
             ]
 
     return colors
 
+
 # Parameters
-saturation_position = 70  # Adjustable saturation level
-hue = 138
-num_colors = 17  # Adjusted for an odd total to test uneven distribution
+saturation_position = 78  # Adjustable saturation level
+hue = 221
+num_colors = 20  # Adjusted for an odd total to test uneven distribution
 
 # Generate colors
 colors = generate_hex_colors(hue, num_colors, saturation_position)
